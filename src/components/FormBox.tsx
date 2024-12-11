9de977b1038b18252beef33a59881a3c97698a81
Riki Itokazu <itokazuriki@gmail.com>
2024-12-11 17:59:24 -0500

    feat: ts union

--- src/components/FormBox.tsx
+++ src/components/FormBox.tsx
@@ -3,20 +3,19 @@ import "./FormBox.scss"
 type LoginProps<T> = {
     fields: LoginFieldProps<T>[]
     className?: string
-}
+} 
 
 type LoginFieldProps<T> = {
-    type: React.HTMLInputTypeAttribute,
     currState: T,
     setCurrState: React.ChangeEventHandler<HTMLInputElement>
     header?: string,
     className?: string, 
-    required?: boolean,
-}
+} & React.InputHTMLAttributes<HTMLInputElement>
 
 export function FormBox<T extends string | number | readonly string[] | undefined>({fields, className}: LoginProps<T>): JSX.Element {
     return (
         <div className = {`formbox ${className}`}>
+            
         {fields?.map((field, index) => (
             <section key = {index} className = "formbox__item">
                 {field.header ?? <header>{field.header}</header>}
@@ -25,6 +24,7 @@ export function FormBox<T extends string | number | readonly string[] | undefine
                     value={field.currState}
                     onChange={field.setCurrState}
                     className = "formbox__box"
+                    {...field}
                 />
             </section>
         ))}
