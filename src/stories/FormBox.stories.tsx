4c862eef8d3bb0f0ca543599d16a0221aca39461
Riki Itokazu <itokazuriki@gmail.com>
2024-12-20 20:58:53 -0800

    refactor

--- src/stories/FormBox.stories.tsx
+++ src/stories/FormBox.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta } from '@storybook/react';
 import { useState } from 'react';
 
-import { FormBox } from '../components/FormBox';
+import { FormBox } from '../components/FormBox/FormBox';
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'FormBox',
